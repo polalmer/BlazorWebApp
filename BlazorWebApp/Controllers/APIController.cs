bff9be1a9bdb3f7fbc3c750d7884c2aff40ba081
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2024-01-25 16:00:39 +0100

    weather with and without api

--- BlazorWebApp/Controllers/APIController.cs
+++ BlazorWebApp/Controllers/APIController.cs
@@ -1,5 +1,5 @@
+using BlazorWebApp.Client.Classes;
 using Microsoft.AspNetCore.Mvc;
-using static BlazorWebApp.Client.Pages.Weather;
 
 namespace BlazorWebApp.Controllers;
 
@@ -10,17 +10,6 @@ public class APIController : ControllerBase
     [HttpGet("Weather")]
     public async Task<List<WeatherForecast>> Get()
     {
-        //Imagine fancy service
-        // Simulate asynchronous loading to demonstrate streaming rendering
-        await Task.Delay(500);
-
-        var startDate = DateOnly.FromDateTime(DateTime.Now);
-        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = startDate.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = summaries[Random.Shared.Next(summaries.Length)]
-        }).ToList();
+        return await WeatherService.GetWeatherForecastsAsync();
     }
 }
---
+++ BlazorWebApp/Controllers/WeatherService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebApp.Client.Classes;
+
+namespace BlazorWebApp.Controllers;
+
+public static class WeatherService
+{
+    private static readonly List<string> summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+    public static async Task<List<WeatherForecast>> GetWeatherForecastsAsync()
+    {
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Count)]
+        }).ToList();
+    }
+}
